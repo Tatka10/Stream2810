d766a3c475669fb06c553558c6847974abf35511
student <9651027@mail.ru>
2024-10-28 18:50:31 +0300

    Stream

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,15 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         Integer [] arr = {1,4,5,6,7};
         ArrayList <Integer> list1 = new ArrayList<>(Arrays.asList(arr));
         System.out.println(list1);
+        Stream <Integer> stream = list1.stream();
+        stream.filter(x->x>5).forEach(System.out::println);//(x-> Sout(x))
 
         }
     }
