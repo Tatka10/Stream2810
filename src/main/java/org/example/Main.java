2cb2f2437dd06d5afe64e90193024a4ba22968cf
student <9651027@mail.ru>
2024-10-28 19:06:02 +0300

    Stream

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,18 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Integer [] arr = {1,4,5,6,7};
-        ArrayList <Integer> list1 = new ArrayList<>(Arrays.asList(arr));
+        Integer[] arr = {1, 4, 5, 6, 7, 8, 9, 4, 5, 4, 3, 7};
+        ArrayList<Integer> list1 = new ArrayList<>(Arrays.asList(arr));
         System.out.println(list1);
-        Stream <Integer> stream = list1.stream();
-        stream.filter(x->x>5).forEach(System.out::println);//(x-> Sout(x))
-
-        }
+        Stream<Integer> stream = list1.stream();
+        Stream<Integer> stream1 = list1.stream();
+        stream.filter(x -> x > 5).forEach(System.out::println);//(x-> Sout(x))
+        //stream1.filter(x->x>5).map(x-> x+1).forEach(System.out::println);//(x-> Sout(x))
+        //stream1.filter(x -> x > 5).limit(3).forEach(System.out::println); //первые 3 элемента
+        //stream1.skip(3).limit(3).forEach(System.out::println);
+        // stream1.sorted().forEach(System.out::println);
+        //stream1.sorted().distinct().forEach(System.out::println);
+        int a= (int) stream1.distinct().count();
+        System.out.println(a);
     }
+}
