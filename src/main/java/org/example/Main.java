9c038c2d0131d6eddd38697661f3d5466328618a
student <9651027@mail.ru>
2024-10-28 18:45:16 +0300

    Stream

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer [] arr = {1,4,5,6,7};
+        ArrayList <Integer> list1 = new ArrayList<>(Arrays.asList(arr));
+        System.out.println(list1);
+
+        }
+    }
